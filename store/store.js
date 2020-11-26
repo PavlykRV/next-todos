eeae3a27ba131cc346179a065591081acb5a9afe
Roman Pavlyk <pavlykrv@gmail.com>
2020-11-26 15:08:14 +0200

    04-add-redux-persist

--- store/store.js
+++ store/store.js
@@ -2,6 +2,29 @@ import {createStore} from 'redux'
 import {createWrapper} from 'next-redux-wrapper'
 import rootReducer from '../reducers/rootReducer'
 
-const makeStore = context => createStore(rootReducer)
+const makeStore = ({ isServer }) => {
+  if (isServer) {
+    return createStore(rootReducer)
+  } else {
+    const { persistReducer, persistStore } = require("redux-persist")
+    const storage = require("redux-persist/lib/storage").default
+
+    const persistConfig = {
+      key: 'nextjs',
+      whitelist: ["ticker", "todos"],
+      storage
+    }
+
+    const presistedReducer = persistReducer(persistConfig, rootReducer)
+
+    const store = createStore(
+      presistedReducer
+    )
+
+    store.__pesistor = persistStore(store)
+
+    return store
+  }
+}
 
 export const wrapper = createWrapper(makeStore, {debug: true});
